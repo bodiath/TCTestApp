c3af8ce2af255744221e358e93db1132860a6865
Bogdan Deshko <bodiath@gmail.com>
2020-08-05 19:03:52 +0300

    Create project

---
+++ GitTutorial/GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Bogdan Deshko on 8/5/20.
+//  Copyright © 2020 JetSoftPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
